268b244e51cbcbec4d97a7655d6d380b777d854c
Lovasi Flórián <lovasifloriian@outlook.hu>
2026-05-03 17:48:28 +0200

    feat: formatters

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateString: string) {
+  const date = new Date(dateString)
+  return date.toLocaleDateString("hu-HU", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  })
+}
+
+export function formatPrice(price: unknown) {
+  if (typeof price !== "number") throw new Error("Price must be a number")
+  return new Intl.NumberFormat("hu-HU", {
+    style: "currency",
+    currency: "HUF",
+  }).format(price)
+}
